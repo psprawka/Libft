a5510511f2c141b0c5b900a4b204e246d4af9785
spraweczga <spraweczga@o2.pl>
2018-06-19 02:05:29 -0700

    Ft_itoa chnaged to stack so it wont leak

--- srcs/numeric/ft_itoa.c
+++ srcs/numeric/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 19:34:42 by psprawka          #+#    #+#             */
-/*   Updated: 2018/06/17 13:18:23 by psprawka         ###   ########.fr       */
+/*   Updated: 2018/06/19 02:03:29 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_itoa(int nbr)
 {
-	int		sign;
-	int		count;
-	char	*str;
+	int			sign;
+	int			count;
+	static char	str[12];
 
 	count = 1;
 	sign = nbr;
@@ -24,10 +24,9 @@ char	*ft_itoa(int nbr)
 		count++;
 	sign = nbr < 0 ? 1 : 0;
 	count = nbr < 0 ? count += 1 : count;
+	str[count] = '\0';
 	if (nbr == -2147483648)
-		return (str = ft_strdup("-2147483648"));
-	if (!(str = ft_strnew(count)))
-		return (NULL);
+		return ("-2147483648");
 	if (sign == 1)
 		str[0] = '-';
 	nbr = nbr < 0 ? nbr *= -1 : nbr;
@@ -36,6 +35,5 @@ char	*ft_itoa(int nbr)
 		str[count] = nbr >= 10 ? (nbr % 10) + 48 : nbr + 48;
 		nbr /= 10;
 	}
-	str[ft_strlen(str)] = '\0';
 	return (str);
 }
