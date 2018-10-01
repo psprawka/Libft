47a53f6fa23146e5d6ca8e11b82a8a90a55f1789
spraweczga <spraweczga@o2.pl>
2018-10-01 14:44:27 -0700

    Add ist front/back added

--- srcs/data_structures/list/add_list.c
+++ srcs/data_structures/list/add_list.c
@@ -6,22 +6,23 @@
 /*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/21 03:35:23 by psprawka          #+#    #+#             */
-/*   Updated: 2018/06/21 09:36:34 by psprawka         ###   ########.fr       */
+/*   Updated: 2018/10/01 14:43:27 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_add_list(t_node **list, void *data)
+int		ft_add_back_list(t_node **list, void *data)
 {
 	t_node *tmp;
 
-	tmp = *list;
-	if (!tmp)
+	if (!(*list))
 	{
 		*list = ft_init_node(data, sizeof(data));
 		return (EXIT_SUCCESS);
 	}
+	
+	tmp = *list;
 	while (tmp->next)
 		tmp = tmp->next;
 
@@ -29,4 +30,18 @@ int		ft_add_list(t_node **list, void *data)
 	return (EXIT_SUCCESS);
 }
 
+int		ft_add_front_list(t_node **list, void *data)
+{
+	t_node *tmp;
 
+	if (!(*list))
+	{
+		*list = ft_init_node(data, sizeof(data));
+		return (EXIT_SUCCESS);
+	}
+	
+	tmp = ft_init_node(data, sizeof(data));
+	tmp->next = *list;
+	*list = tmp;
+	return (EXIT_SUCCESS);
+}
