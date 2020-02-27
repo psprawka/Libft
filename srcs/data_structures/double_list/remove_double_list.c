f19593d518850fe30014290f9748394a867e6cb6
Paulina Sprawka <spraweczga@gmail.com>
2020-02-27 18:58:14 +0100

    double linked list fixed a bit

--- srcs/data_structures/double_list/remove_double_list.c
+++ srcs/data_structures/double_list/remove_double_list.c
@@ -21,8 +21,13 @@ void	ft_remove_double_list(t_dnode **list, t_dnode *node)
 	{
 		if (tmp == node)
 		{
+			//printf("PREV: %p, curr: %p, next: %p\n", tmp->prev, tmp, tmp->next);
 			if (!tmp->prev)
+			{
+				if (tmp->next)
+					tmp->next->prev = NULL;
 				*list = tmp->next;
+			}
 			else
 				tmp->prev->next = tmp->next;
 			free(tmp);
